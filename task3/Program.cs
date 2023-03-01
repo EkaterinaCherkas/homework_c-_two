e5d87ec188825df1b55e161111d708356100ec8e
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-01 20:25:13 +0300

    add task about dayoftheweek

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Clear();
+
+Console.WriteLine("Введите цифру дня недели");
+int dayofweek = int.Parse(Console.ReadLine());
+if (dayofweek <= 5){
+    Console.WriteLine("no");
+}
+else {
+    Console.WriteLine("yes");
+}
