da7ff8b3e43e9b5c8bc0a8a19a425654102229aa
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-01 20:12:47 +0300

    add task1 программа показывает 2 цифру трехзначного числа

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+
+int number = new Random().Next(100 , 1000);
+Console.WriteLine(number);
+int a = number / 100 ; // 918 / 10 = 91 /100 = 9 %10 = 8 %100 = 18
+int b = number %100 /10;
+int c = number % 10;
+Console.WriteLine(b);
