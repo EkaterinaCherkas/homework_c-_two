2a234452d33d5b119d21d455a43e7954dcc18fd0
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-01 22:00:46 +0300

    add task выводит третью цифру заданного числа

--- task2/Program.cs
+++ task2/Program.cs
@@ -7,3 +7,13 @@ Console.Clear();
 
 Console.WriteLine("Введите число");
 int num = int.Parse(Console.ReadLine());
+if (num < 100){
+    Console.WriteLine("В числе нет третьей цифры");
+}
+// можно без else если после этой строчки написать return
+else{
+    while (num > 999){
+        num = num /10;
+    }
+    Console.WriteLine("Третья цифра " + num%10);
+}
